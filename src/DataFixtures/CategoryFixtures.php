2bead7173c412e30d37223fee8e3f0933f62b3f8
Aurore-T <tellier.aurore@outlook.fr>
2026-02-17 12:09:15 +0100

    fixtures for product

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $categories = ['T-shirt', 'Pantalon', 'Chaussure', 'Chaussettes', 'Sous-vêtement', 'Accessoire'];
+
+        foreach ($categories as $key => $value) {
+            $category = new Category();
+
+            $category->setTitle ($value);
+
+            $manager->persist($category);
+
+            $this->addReference('category-' . $key, $category);
+
+        }
+
+        $manager->flush();
+
+    }
+}
